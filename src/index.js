f4f7905b247d87f6765271fc9b964d65720d2780
shadrach akaade <akaadesm@gmail.com>
2022-12-06 21:21:37 +0100

    fix stylelint error

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-//mport App from "./components/App";
 import "./index.css";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
