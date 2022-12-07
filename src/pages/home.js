f67dcf581397acf57b29fd5e6cec8caffd83bb18
shadrach akaade <akaadesm@gmail.com>
2022-12-08 00:06:33 +0100

    fix stylelint error

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,13 +7,13 @@ import { useEffect, useState } from "react";
 import { setCountry } from "../redux/actions";
 import Countrylist from "../components/countrylist";
 import { Link } from "react-router-dom";
-import virus from "../assets/conifer-virus.png"
+import virus from "../assets/conifer-virus.png";
 
 const Home = () => {
-const [continents, setContinents] = useState([])
-const [selectedContinent, setSelectedContinent] = useState("");
-const [filtered, setFiltered] = useState([]);
-const dispatch = useDispatch()
+  const [continents, setContinents] = useState([]);
+  const [selectedContinent, setSelectedContinent] = useState("");
+  const [filtered, setFiltered] = useState([]);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     axios
@@ -22,8 +22,9 @@ const dispatch = useDispatch()
       .catch((err) => console.log(err));
     axios
       .get("https://disease.sh/v3/covid-19/countries")
-      .then((res) => dispatch(setCountry(res.data))
-      .catch((err) => console.log(err)));
+      .then((res) =>
+        dispatch(setCountry(res.data)).catch((err) => console.log(err))
+      );
   }, []);
 
   useEffect(() => {
@@ -37,45 +38,51 @@ const dispatch = useDispatch()
     }
   }, [selectedContinent]);
 
-  
   return (
     <>
       <Homenavbar />
-      <Allcases/>
+      <Allcases />
+
       <span className="flex justify-end py-2 pr-3">
-      <select className=" bg-purple-700 rounded-lg p-2 text-white"
-        onChange={(e) => setSelectedContinent(e.target.value)}
-      >
-        <option value="">Filter by continent</option>
-        {continents?.map((con) => (
-          <option value={con.continent} key={con.continent}>
-            {con.continent}{" "}
-          </option>
-        ))}
-      </select></span>
-      {filtered.length
-          ? filtered.map((fil) => <div
-        className="container-fluid col-md-4 col-lg-3 text-center"
-        key={fil}
-      >
-        <Link to={`/country/${fil}`}>
-          <div className="">
-            <div className="my-2 text-center">
-              <div className="rounded-lg py-5 bg-purple-700 shadow-lg shadow-purple-700/50">
-                <div className="m-auto flex justify-center">
-                  <img src={virus} className="w-25 virus" alt="flag"></img>
-                </div>
-                <div className="text-white py-2">
-                  <h6 className="card-title text-lg font-semibold">
-                    {fil}
-                  </h6>
+        <select
+          className=" bg-purple-700 rounded-lg p-2 text-white"
+          onChange={(e) => setSelectedContinent(e.target.value)}
+        >
+          <option value="">Filter by continent</option>
+          {continents?.map((con) => (
+            <option value={con.continent} key={con.continent}>
+              {con.continent}{" "}
+            </option>
+          ))}
+        </select>
+      </span>
+      {filtered.length ? (
+        filtered.map((fil) => (
+          <div
+            className="container-fluid col-md-4 col-lg-3 text-center"
+            key={fil}
+          >
+            <Link to={`/country/${fil}`}>
+              <div className="">
+                <div className="my-2 text-center">
+                  <div className="rounded-lg py-5 bg-purple-700 shadow-lg shadow-purple-700/50">
+                    <div className="m-auto flex justify-center">
+                      <img src={virus} className="w-25 virus" alt="flag"></img>
+                    </div>
+                    <div className="text-white py-2">
+                      <h6 className="card-title text-lg font-semibold">
+                        {fil}
+                      </h6>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
+            </Link>
           </div>
-        </Link>
-      </div>)
-          : <Countrylist/>}
+        ))
+      ) : (
+        <Countrylist />
+      )}
     </>
   );
 };
